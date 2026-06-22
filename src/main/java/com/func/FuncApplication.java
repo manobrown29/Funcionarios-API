c01abbe1ff9e11145e0954274c0aebd8ea85212c
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-22 14:48:39 -0300

    Inicio do projeto

---
+++ src/main/java/com/func/FuncApplication.java
@@ -0,0 +1,13 @@
+package com.func;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncApplication.class, args);
+	}
+
+}
